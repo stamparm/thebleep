56b25e453c90d4e28fa96161013632da105e6a16
Miroslav Stampar <miroslav.stampar@gmail.com>
2026-08-29 23:08:20 +0200

    Guard root removal suggestions

--- tests/rules/test_rm_root.py
+++ tests/rules/test_rm_root.py
@@ -2,14 +2,30 @@ import pytest
 from thebleep.rules.rm_root import match, get_new_command
 from thebleep.types import Command
 
+OUTPUT = ("rm: it is dangerous to operate recursively on '/'\n"
+          "rm: use --no-preserve-root to override this failsafe")
+
 
 def test_match():
-    assert match(Command('rm -rf /', 'add --no-preserve-root'))
+    assert match(Command('rm -rf /', OUTPUT))
+
+
+def test_sudo_rm_still_matches():
+    assert match(Command('sudo rm -rf /', OUTPUT))
+
+
+@pytest.mark.parametrize('script', [
+    'echo rm /',
+    'git rm /',
+    'python rm /',
+])
+def test_words_in_another_command_are_not_a_root_removal(script):
+    assert not match(Command(script, OUTPUT))
 
 
 @pytest.mark.parametrize('command', [
-    Command('ls', 'add --no-preserve-root'),
-    Command('rm --no-preserve-root /', 'add --no-preserve-root'),
+    Command('ls', OUTPUT),
+    Command('rm --no-preserve-root /', OUTPUT),
     Command('rm -rf /', '')])
 def test_not_match(command):
     assert not match(command)
--- thebleep/rules/rm_root.py
+++ thebleep/rules/rm_root.py
@@ -1,13 +1,14 @@
 from thebleep.specific.sudo import sudo_support
+from thebleep.utils import is_app
 
 enabled_by_default = False
 
 
 @sudo_support
 def match(command):
-    return (command.script_parts
-            and {'rm', '/'}.issubset(command.script_parts)
-            and '--no-preserve-root' not in command.script
+    return (is_app(command, 'rm', at_least=1)
+            and '/' in command.script_parts
+            and '--no-preserve-root' not in command.script_parts
             and '--no-preserve-root' in command.output)
 
 
