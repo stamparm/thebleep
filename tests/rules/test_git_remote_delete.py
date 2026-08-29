137a66491b0956324ec4d6097cbccb64adff965c
Miroslav Stampar <miroslav.stampar@gmail.com>
2026-08-29 21:38:59 +0200

    Tighten git remote matching

--- tests/rules/test_git_remote_delete.py
+++ tests/rules/test_git_remote_delete.py
@@ -16,9 +16,19 @@ def test_not_match(command):
     assert not match(command)
 
 
+def test_not_match_when_remote_delete_is_configuration_data():
+    assert not match(Command('git config remote.delete "remote delete"', ''))
+
+
 @pytest.mark.parametrize('command, new_command', [
     (Command('git remote delete foo', ''), 'git remote remove foo'),
     (Command('git remote delete delete', ''), 'git remote remove delete'),
 ])
 def test_get_new_command(command, new_command):
     assert get_new_command(command) == new_command
+
+
+def test_get_new_command_preserves_text_in_a_remote_name():
+    command = Command('DELETE=delete git remote delete foo', '')
+
+    assert get_new_command(command) == 'DELETE=delete git remote remove foo'
--- thebleep/rules/git_remote_delete.py
+++ thebleep/rules/git_remote_delete.py
@@ -1,13 +1,14 @@
-import re
-
 from thebleep.specific.git import git_support
+from thebleep.utils import command_word_index, replace_argument
 
 
 @git_support
 def match(command):
-    return "remote delete" in command.script
+    parts = command.script_parts
+    index = command_word_index(parts)
+    return parts[index + 1:index + 3] == ['remote', 'delete']
 
 
 @git_support
 def get_new_command(command):
-    return re.sub(r"delete", "remove", command.script, count=1)
+    return replace_argument(command.script, 'delete', 'remove')
